3be2363b5d26e51a6ba552fb8afc178f1bcde6a9
haripriyaprathyush <haripriya.th@gmail.com>
2024-09-09 15:19:19 +0530

    amazon work

---
+++ amazon/src/amazonpkg/Work.java
@@ -0,0 +1,16 @@
+package amazonpkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Work {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+
+	}
+
+}
